f98d13bd0c9b3fd96cec9ef088f49e8aab94e420
Adrien RICHARD <richardadrien0@gmail.com>
2023-02-26 10:23:38 +0100

    more code

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-function generate() {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            // Do something with the JSON data
-            // Get a random index from the JSON data array
-            const randomIndex = Math.floor(Math.random() * data.length);
+var ingredients = [
+    { id: 1, name: "Tomato" }
+]
+var food = [
+    { id: 1, name: 'Pizza', ingredients: [1] },
+    { id: 2, name: 'Jane' },
+    { id: 3, name: 'Bob' },
+    { id: 4, name: 'Alice' },
+    { id: 5, name: 'Mike' }
+]
 
-            // Get the item at the random index
-            const randomItem = data[randomIndex];
+function generate(numberToGenerate) {
+    var awenser = []
+    for (let i = 0; i < numberToGenerate; i++) {
+        awenser.push(getRandom())
+    }
 
-            // Log the random item to the console
-            console.log(randomItem);
-        })
-        .catch(error => console.error(error));
+    populatehtml(awenser)
+    console.log(awenser)
+}
+
+function getRandom() {
+    const randomIndex = Math.floor(Math.random() * food.length);
+    return food[randomIndex];
+}
+
+function populatehtml(list) {
+    list.forEach(element => {
+        document.getElementById(list).innerHTML += `<li>${element.name}</li>`
+    });
 }
